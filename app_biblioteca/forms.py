316eea94ee6ff31cc16fab0e44a1b00b037123e4
Cristian Nunes <crisjav2812@gmail.com>
2022-08-11 01:43:16 -0300

    posteos listo

--- app_biblioteca/forms.py
+++ app_biblioteca/forms.py
@@ -62,11 +62,12 @@ class PasswordChangeForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('__all__')
+        fields = ('titulo','autor','body')
         
-        widget = {
+        widgets = {
             'titulo': forms.TextInput(attrs={'class':'form-control'}),
-            'autor': forms.Select(attrs={'class':'form-control'}),
+            'autor': forms.TextInput(attrs={'class':'form-control', 'value':'','id':'elder','type':'hidden'}),
+            #'autor': forms.Select(attrs={'class':'form-control')
             'body': forms.Textarea(attrs={'class':'form-control'}),
         
         }   
